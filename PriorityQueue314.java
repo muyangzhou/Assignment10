36a869138d1094e9db76abe17710de525a3b3751
Muyang Zhou <165830427+muyangzhou@users.noreply.github.com>
2024-11-23 21:06:20 -0600

    Update PriorityQueue314.java

--- PriorityQueue314.java
+++ PriorityQueue314.java
@@ -1,8 +1,18 @@
+/**
+ * A PriorityQueue class that implements consistent placement for equivalent
+ * objects
+ */
 public class PriorityQueue314<T extends Comparable<T>> {
+    /**
+     * Node class that allows LinkedList-style implementation of the PQ314
+     */
     private static class Node<T> {
         T value;
         Node<T> next;
 
+        /**
+         * @param value value to be stored by this Node
+         */
         Node(T value) {
             this.value = value;
             this.next = null;
@@ -12,31 +22,42 @@ public class PriorityQueue314<T extends Comparable<T>> {
     private Node<T> front;
     private int size;
 
+    /**
+     * Default constructor of PQ314 that creates empty PQ314
+     */
     public PriorityQueue314() {
         this.front = null;
         size = 0;
     }
 
-public void enqueue(T value) {
-    //if not inserts new node w priority 1
-    Node<T> newNode = new Node<>(value);
+    /**
+     * Adds value to end of the queue
+     * @param value value to add to end of the queue
+     */
+    public void enqueue(T value) {
+        // if not inserts new node w priority 1
+        Node<T> newNode = new Node<>(value);
 
-    if (front == null || front.value.compareTo(newNode.value) > 0) {
-        newNode.next = front;
-        front = newNode;
-    } else {
-        Node<T> current = front;
+        if (front == null || front.value.compareTo(newNode.value) > 0) {
+            newNode.next = front;
+            front = newNode;
+        } else {
+            Node<T> current = front;
 
-        while (current.next != null && current.next.value.compareTo(newNode.value) <= 0) {
-            current = current.next;
+            while (current.next != null && current.next.value.compareTo(newNode.value) <= 0) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
         }
-        newNode.next = current.next;
-        current.next = newNode;
+
+        size++;
     }
 
-    size++;
-}
-
+    /**
+     * Retrieves and removes the value at the front of the queue
+     * @return the value at the front of the queue
+     */
     public T dequeue() {
         if (front == null) {
             throw new IllegalStateException("front is null");
@@ -47,6 +68,10 @@ public void enqueue(T value) {
         return value;
     }
 
+    /**
+     * Retrives the value at the front of the queue
+     * @return the value at the front of the queue
+     */
     public T peek() {
         if (front == null) {
             throw new IllegalStateException("front is null");
@@ -54,10 +79,17 @@ public void enqueue(T value) {
         return front.value;
     }
 
+    /**
+     * @return size of the queue
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Prints the queue in format [data] -> [data] -> ... -> null
+     * For debugging purposes
+     */
     public void print() {
         Node<T> current = front;
         while (current != null) {
